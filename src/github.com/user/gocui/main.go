5925ae5e2f330dc8f94c21e1ef93691e67c1efb0
Fedoruk Yaroslav <y.fedoruk@favbet.com>
2018-04-08 16:07:18 +0300

    --

---
+++ src/github.com/user/gocui/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jroimartin/gocui"
+)
+
+func main() {
+	g, err := gocui.NewGui(gocui.OutputNormal)
+
+	if err != nil {
+		log.Println(err)
+	}
+	defer g.Close()
+
+	g.SetManagerFunc(layout)
+
+	g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit)
+	g.MainLoop()
+
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("hello", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
+		fmt.Println(v, "Hello world!")
+
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
