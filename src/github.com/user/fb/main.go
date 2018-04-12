218ec7e49016dcae210f88f8ab4deccc0bdaf83d
Fedoruk Yaroslav <y.fedoruk@favbet.com>
2018-04-12 18:08:39 +0300

    --

--- src/github.com/user/fb/main.go
+++ src/github.com/user/fb/main.go
@@ -7,9 +7,20 @@ import (
 )
 
 func main() {
-	res, _ := fb.Get("/100001773162306", fb.Params{
-		"fields":       "first_name",
-		"access_token": "EAACEdEose0cBAIoqCOBM00MdZAu2xZB3VZAD5qkLz1YjASIIg8O20ZBkjmtELG4k8KTQd0MGy5EFBWdTS69buVC8fZCvsJ6oyCsxpdM32u3JDsDvOTgDHj5L5eZCnndKutjYOeFD13gxzVky2M1vTyWiHoc0T6ix0pUT9UEkRDq5z4dtplcLzHHJNATcqcB78ZD",
+	tok := "EAACEdEose0cBAIoqCOBM00MdZAu2xZB3VZAD5qkLz1YjASIIg8O20ZBkjmtELG4k8KTQd0MGy5EFBWdTS69buVC8fZCvsJ6oyCsxpdM32u3JDsDvOTgDHj5L5eZCnndKutjYOeFD13gxzVky2M1vTyWiHoc0T6ix0pUT9UEkRDq5z4dtplcLzHHJNATcqcB78ZD"
+	res, _ := fb.Get("/831976246906620/feed", fb.Params{
+		"access_token": tok,
 	})
-	fmt.Println(res["first_name"])
+
+	var items []fb.Result
+	err := res.DecodeField("data", &items)
+
+	if err != nil {
+		fmt.Printf("An error has happened %v", err)
+		return
+	}
+
+	for _, item := range items {
+		fmt.Println(item["message"])
+	}
 }
